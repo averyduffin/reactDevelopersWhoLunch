7d25ea6f8d6019f899114869e751aa879b4ecefe
Avery Duffin <avery.duffin@rainfoucus.com>
2019-08-10 17:48:07 -0600

    adding all the packages

--- src/App.js
+++ src/App.js
@@ -9,9 +9,22 @@ import Preview from './Preview';
 import NotFound from './components/NotFound';
 import Post from './components/Post';
 import BlogHome from './components/BlogHome';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+
+const history = createBrowserHistory();
+
+const trackingId = "UA-144099775-2";
+ReactGA.initialize(trackingId);
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const App = (props) => (
-  <Router>
+  <Router history={history}>
     <Switch>
       <Redirect exact from="/blog/" to="/" />
       <Route exact path="/" render={routeProps => <BlogHome{...routeProps} prismicCtx={props.prismicCtx} />} />
